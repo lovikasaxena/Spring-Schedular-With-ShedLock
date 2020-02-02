75a2e1d431e4e17e9c169c27ae1dbf272055a636
lovikasaxena <slovika@thoughtworks.com>
2020-02-02 18:48:33 +0530

    Lovika - Update cron in FirstSchedular

--- src/main/kotlin/com/lovikasaxena/shedlock/schedular/FirstSchedular.kt
+++ src/main/kotlin/com/lovikasaxena/shedlock/schedular/FirstSchedular.kt
@@ -3,12 +3,14 @@ package com.lovikasaxena.shedlock.schedular
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 
+@Component
 class FirstSchedular {
 
     private val log = LoggerFactory.getLogger(FirstSchedular::class.java)
 
-    @Scheduled(cron = "0 0 9 * * *")    /* Schedule this task everyday att 9am */
+    @Scheduled(cron = "0 0 */1 ? * *")    /* Schedule this task everyday at 9am */
     @SchedulerLock(name = "scheduledTaskName", lockAtMostFor = "30m", lockAtLeastFor = "20m")
     fun schedule(){
         log.info("Running the scheduled task: $i++, using thread: ${Thread.currentThread().name}")
